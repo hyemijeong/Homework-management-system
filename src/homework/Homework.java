4fb52804a39d1ec3abc320f8a8b8c7b556874c80
hyemijeong <62432916+hyemijeong@users.noreply.github.com>
2020-04-23 11:24:10 +0900

    Update Homework.java

--- src/homework/Homework.java
+++ src/homework/Homework.java
@@ -1,10 +1,48 @@
-package homework;
+package homework2;
+
+import java.util.Scanner;
 
 public class Homework {
-	String subject;
-	String deadline;
-	String content;
+
+	protected Homeworkkind kind=Homeworkkind.Physics;
+	protected String subject;
+	protected String deadline;
+	protected String content;
+	//다른 프로그램에서는 접근할 수 없도록 encapsulation을 함.
 	
+	public Homeworkkind getKind() {
+		return kind;
+	}
+
+	public void setKind(Homeworkkind kind) {
+		this.kind = kind;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+    // get함수를 이용하여 외부의 다른 클래스에서 변수를 이용할 수 있도록하고, set함수를 이용하여 다른 클래스에서 변수의 값을 바꿀 수 있게함.
 	public Homework() {
 	}	
 	
@@ -24,4 +62,18 @@ public class Homework {
 		System.out.println("subject:" +subject+",deadline:"+deadline+",content:"+content);
 	}
 	
+	public void getUserInput(Scanner input) {
+		System.out.println("Subject :");
+		String subject=input.next();
+		this.setSubject(subject);
+		
+		System.out.println("Deadline :");
+		String deadline=input.next();
+		this.setDeadline(deadline);
+		
+		System.out.println("Content :");
+		String content=input.next();
+		this.setContent(content);
+		
+	}//각각의 객체의 값을 입력받아 그 값을 저장하는 함수.
 }
